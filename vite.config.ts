d9fe541ee82509200f6ce3f0d6451de541175b95
이용운 <ywlee@wbsoft.kr>
2022-11-18 10:44:03 +0900

    라우터 및 세팅 업데이트

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import * as path from 'path'
+import { resolve } from 'path'
 
 export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, './src/main.ts'),
+      name: 'MyLib',
+      fileName: (format) => `vite.${format}.js`
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname,'./src')
